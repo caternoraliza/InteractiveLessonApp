08968afd7565997339877c362d5e8287f291f5d2
Aliza Caternor <caternoraliza@gmail.com>
2024-06-21 13:21:26 -0400

    Initial Commit

---
+++ InteractiveLessonApp/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  InteractiveLessonApp
+//
+//  Created by Scholar on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    @State private var titleText =
+    "What is your name?"
+    var body: some View {
+        VStack (spacing: 100) {
+            Text(titleText)
+                .font(.largeTitle)
+            TextField("Type your name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.black, width: 2)
+            
+            Button("Submit Name") {
+                titleText = "Welcome, \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+            @State private var titleText =
+            Text("What is your age")
+                
+                
+            }
+
+        
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveLessonApp/InteractiveLessonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveLessonAppApp.swift
+//  InteractiveLessonApp
+//
+//  Created by Scholar on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveLessonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
